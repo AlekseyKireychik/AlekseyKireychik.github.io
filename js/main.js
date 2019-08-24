e0fad17abd8dace079505a64a5d37ca43952542d
AlekseyKireychik <kireychika@mail.ru>
2019-08-24 14:14:31 +0300

    del js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload=function(){
-    var messageEle=document.getElementById('message');
-    function receiveMessage(e){
-        if(e.origin!=="file:///C:/Users/Aleksey/Desktop/testJS/index.html")
-        return;
-        messageEle.innerHTML="Message Received: "+e.data;
-    }
-    window.addEventListener('message',receiveMessage);
-}
